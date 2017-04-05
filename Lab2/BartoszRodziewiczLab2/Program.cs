92f89f997ca4a956a332e4efb980a6bb20f78b8f
bartosz-rodziewicz <bartoszka1996@gmail.com>
2017-04-05 19:21:30 +0200

    usuniecie zbednych importow

--- Lab2/BartoszRodziewiczLab2/Program.cs
+++ Lab2/BartoszRodziewiczLab2/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BartoszRodziewiczLab2 {
+namespace BartoszRodziewiczLab2
+{
     class Program {
         static void Main(string[] args) {
             Car myCar = new Car();
